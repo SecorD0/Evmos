b14ddc8ee38964c2a30fc58893cd318346eba8e6
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-11-17 22:38:06 +0300

    Performs many functions

--- multi_tool.sh
+++ multi_tool.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Default variables
-type="install"
+function="install"
+
 # Options
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
 option_value(){ echo "$1" | sed -e 's%^--[^=]*=%%g; s%^-[^=]*=%%g'; }
@@ -24,7 +25,7 @@ while test $# -gt 0; do
 		return 0 2>/dev/null; exit 0
 		;;
 	-u|--update)
-		type="update"
+		function="update"
 		shift
 		;;
 	*|--)
@@ -32,6 +33,7 @@ while test $# -gt 0; do
 		;;
 	esac
 done
+
 # Functions
 printf_n(){ printf "$1\n" "${@:2}"; }
 install() {
@@ -39,40 +41,24 @@ install() {
 	sudo apt upgrade -y
 	sudo apt install wget git build-essential make jq -y
 	mkdir $HOME/evmos_temp
-	evmos_version=`wget -qO- https://api.github.com/repos/tharsis/evmos/releases/latest | jq -r ".tag_name" | sed "s%v%%g"`
+	local evmos_version=`wget -qO- https://api.github.com/repos/tharsis/evmos/releases/latest | jq -r ".tag_name" | sed "s%v%%g"`
 	wget -q "https://github.com/tharsis/evmos/releases/download/v${evmos_version}/evmos_${evmos_version}_Linux_x86_64.tar.gz"
 	tar -xvf "evmos_${evmos_version}_Linux_x86_64.tar.gz" -C $HOME/evmos_temp
-	mv $HOME/evmos_temp/bin/evmosd /usr/bin/evmosd
+	mv $HOME/evmos_temp/bin/evmosd /usr/bin
 	rm -rf $HOME/evmos_temp "evmos_${evmos_version}_Linux_x86_64.tar.gz"
-	evmosd version
-	printf "[Unit]
-Description=Evmos Daemon
-After=network-online.target
-
-[Service]
-User=$USER
-ExecStart=`which evmosd` start
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/evmosd.service
-	sudo systemctl daemon-reload
-	sudo systemctl enable evmosd
-	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n evmos_log -v "sudo journalctl -f -n 100 -u evmosd" -a
-	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n evmos_node_info -v ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Evmos/main/node_info.sh) -l RU 2> /dev/null" -a
 }
-# Actions
-sudo apt install wget -y &>/dev/null
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
-
-if [ "$type" = "update" ]; then
+update() {
 	sudo systemctl stop evmosd
-	rm -rf `which evmosd`
-	install
-	sudo systemctl restart evmosd
-else
-	install
-fi
+	mkdir $HOME/evmos_temp
+	local evmos_version=`wget -qO- https://api.github.com/repos/tharsis/evmos/releases/latest | jq -r ".tag_name" | sed "s%v%%g"`
+	wget -q "https://github.com/tharsis/evmos/releases/download/v${evmos_version}/evmos_${evmos_version}_Linux_x86_64.tar.gz"
+	tar -xvf "evmos_${evmos_version}_Linux_x86_64.tar.gz" -C $HOME/evmos_temp
+	mv $HOME/evmos_temp/bin/evmosd /usr/bin
+	rm -rf $HOME/evmos_temp "evmos_${evmos_version}_Linux_x86_64.tar.gz"
+}
+
+# Actions
+sudo apt install wget -y
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
+$function
 printf_n "${C_LGn}Done!${RES}"
