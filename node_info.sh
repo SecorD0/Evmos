638c379fbacb2759a48af72f3cf8aff482a4142b
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-12-01 00:32:55 +0300

    Shows node info

--- node_info.sh
+++ node_info.sh
@@ -7,7 +7,7 @@ wallet_name="$evmos_wallet_name"
 wallet_address="$evmos_wallet_address"
 wallet_address_variable="evmos_wallet_address"
 global_rpc="http://5.189.156.65:26657/"
-explorer_url_template=""
+explorer_url_template="https://explorer.evmos.nodes.guru/validator/"
 
 # Default variables
 language="EN"
@@ -142,7 +142,7 @@ main() {
 	local catching_up=`jq -r ".SyncInfo.catching_up" <<< $status`
 
 	local validator_address=`jq -r ".operator_address" <<< $node_info`
-	#if [ -n "$validator_address" ]; then local explorer_url="${explorer_url_template}${validator_address}"; fi
+	if [ -n "$validator_address" ]; then local explorer_url="${explorer_url_template}${validator_address}"; fi
 	local validator_pub_key=`$daemon tendermint show-validator | tr "\"" "'"`
 	local jailed=`jq -r ".jailed" <<< $node_info`
 	local delegated=`bc -l <<< "$(jq -r ".tokens" <<< $node_info)/1000000000000000000"`
