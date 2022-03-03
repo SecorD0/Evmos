3b57d6437206ceb6e078699ff86b76646bf2df31
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-03-03 19:56:11 +0300

    Shows node info

--- node_info.sh
+++ node_info.sh
@@ -6,7 +6,9 @@ node_dir="$HOME/.evmosd/"
 wallet_name="$evmos_wallet_name"
 wallet_address="$evmos_wallet_address"
 wallet_address_variable="evmos_wallet_address"
-global_rpc=""
+validator_address="$evmos_validator_address"
+validator_address_variable="evmos_validator_address"
+global_rpc="https://tendermint.bd.evmos.org:26657/"
 explorer_url_template="https://www.mintscan.io/evmos/validators/"
 
 # Default variables
@@ -61,7 +63,7 @@ printf_n(){ printf "$1\n" "${@:2}"; }
 main() {
 	# Texts
 	if [ "$language" = "RU" ]; then
-		local t_ewa="Для просмотра баланса кошелька необходимо добавить его в систему виде переменной, поэтому ${C_LGn}введите пароль от кошелька${RES}"
+		local t_ewa="Для просмотра баланса кошелька необходимо добавить его в систему виде переменной, поэтому ${C_LGn}введите пароль от кошелька${RES}: "
 		local t_ewa_err="${C_LR}Не удалось получить адрес кошелька!${RES}"
 		local t_nn="\nНазвание ноды:              ${C_LGn}%s${RES}"
 		local t_id="Keybase ключ:               ${C_LGn}%s${RES}"
@@ -76,7 +78,8 @@ main() {
 		local t_sy2="Осталось нагнать:           ${C_LR}%d-%d=%d (около %.2f мин.)${RES}"
 		local t_sy3="Нода синхронизирована:      ${C_LGn}да${RES}"
 		
-		local t_va="\nАдрес валидатора:           ${C_LGn}%s${RES}"
+		local t_vm="\nНазвание валидатора:        ${C_LGn}%s${RES}"
+		local t_va="Адрес валидатора:           ${C_LGn}%s${RES}"
 		local t_eu="Страница в эксплорере:      ${C_LGn}%s${RES}"
 		local t_pk="Публичный ключ валидатора:  ${C_LGn}%s${RES}"
 		local t_nij1="Валидатор в тюрьме:         ${C_LR}да${RES}"
@@ -89,7 +92,7 @@ main() {
 	# Send Pull request with new texts to add a language - https://github.com/SecorD0/Evmos/blob/main/node_info.sh
 	#elif [ "$language" = ".." ]; then
 	else
-		local t_ewa="To view the wallet balance, you have to add it to the system as a variable, so ${C_LGn}enter the wallet password${RES}"
+		local t_ewa="To view the wallet balance, you have to add it to the system as a variable, so ${C_LGn}enter the wallet password${RES}: "
 		local t_ewa_err="${C_LR}Failed to get the wallet address!${RES}"
 		local t_nn="\nMoniker:                 ${C_LGn}%s${RES}"
 		local t_id="Keybase key:             ${C_LGn}%s${RES}"
@@ -104,7 +107,8 @@ main() {
 		local t_sy2="It remains to catch up:  ${C_LR}%d-%d=%d (about %.2f min.)${RES}"
 		local t_sy3="Node is synchronized:    ${C_LGn}yes${RES}"
 		
-		local t_va="\nValidator address:       ${C_LGn}%s${RES}"
+		local t_vm="\nValidator moniker:       ${C_LGn}%s${RES}"
+		local t_va="Validator address:       ${C_LGn}%s${RES}"
 		local t_eu="Page in explorer:        ${C_LGn}%s${RES}"
 		local t_pk="Validator public key:    ${C_LGn}%s${RES}"
 		local t_nij1="Validator in a jail:     ${C_LR}yes${RES}"
@@ -118,19 +122,24 @@ main() {
 
 	# Actions
 	sudo apt install bc -y &>/dev/null
-	if [ -n "$wallet_name" ] && [ ! -n "$wallet_address" ]; then
-		printf_n "$t_ewa"
-		local wallet_address=`$daemon keys show "$wallet_name" -a`
+	if [ -n "$wallet_name" ] && ([ ! -n "$wallet_address" ] || [ ! -n "$validator_address" ]); then
+		printf "$t_ewa"
+		local password
+		read password
+		local wallet_address=`echo "$password" | $daemon keys show "$wallet_name" -a`
 		if [ -n "$wallet_address" ]; then
+			local validator_address=`echo "$password" | $daemon keys show "$wallet_name" -a --bech val`
 			. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n "$wallet_address_variable" -v "$wallet_address"
+			. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n "$validator_address_variable" -v "$validator_address"
 		else
 			printf_n "$t_ewa_err"
 		fi
+		unset password
 	fi
 	local local_rpc=`grep -oPm1 "(?<=^laddr = \")([^%]+)(?=\")" "${node_dir}config/config.toml"`
 	local status=`$daemon status --node "$local_rpc" 2>&1`
 	local moniker=`jq -r ".NodeInfo.moniker" <<< $status`
-	local node_info=`$daemon query staking validators --node "$local_rpc" --limit 10000 --output json | jq -r '.validators[] | select(.description.moniker=='\"$moniker\"')'`
+	local node_info=`$daemon query staking validators --node "$local_rpc" --limit 10000 -oj | jq -r '.validators[] | select(.operator_address=='\"$validator_address\"')'`
 	local identity=`jq -r ".description.identity" <<< $node_info`
 	local website=`jq -r ".description.website" <<< $node_info`
 	local details=`jq -r ".description.details" <<< $node_info`
@@ -140,20 +149,20 @@ main() {
 	local node_version=`$daemon version`
 	local latest_block_height=`jq -r ".SyncInfo.latest_block_height" <<< $status`
 	local catching_up=`jq -r ".SyncInfo.catching_up" <<< $status`
-
-	local validator_address=`jq -r ".operator_address" <<< $node_info`
+	
+	local validator_moniker=`jq -r ".description.moniker" <<< $node_info`
 	if [ -n "$validator_address" ]; then local explorer_url="${explorer_url_template}${validator_address}"; fi
 	local validator_pub_key=`$daemon tendermint show-validator | tr "\"" "'"`
 	local jailed=`jq -r ".jailed" <<< $node_info`
 	local delegated=`bc -l <<< "$(jq -r ".tokens" <<< $node_info)/1000000000000000000"`
 	local voting_power=`bc -l <<< "$(jq -r ".ValidatorInfo.VotingPower" <<< $status)/1000000000000"`
 	if [ -n "$wallet_address" ]; then
-		local balance=`bc -l <<< "$($daemon query bank balances "$wallet_address" -o json --node "$global_rpc" | jq -r ".balances[0].amount")/1000000000000000000"`
+		local balance=`bc -l <<< "$($daemon query bank balances "$wallet_address" -oj --node "$global_rpc" | jq -r ".balances[0].amount")/1000000000000000000"`
 	fi
 
 	# Output
 	if [ "$raw_output" = "true" ]; then
-		printf_n '{"moniker": "%s", "identity": "%s", "website": "%s", "details": "%s", "network": "%s", "node_id": "%s", "node_version": "%s", "latest_block_height": %d, "catching_up": %b, "validator_address": "%s", "explorer_url": "%s", "validator_pub_key": "%s", "jailed": %b, "delegated": %.4f, "voting_power": %.4f, "wallet_address": "%s", "balance": %.4f}' \
+		printf_n '{"moniker": "%s", "identity": "%s", "website": "%s", "details": "%s", "network": "%s", "node_id": "%s", "node_version": "%s", "latest_block_height": %d, "catching_up": %b, "validator_moniker": "%s", "validator_address": "%s", "explorer_url": "%s", "validator_pub_key": "%s", "jailed": %b, "delegated": %.4f, "voting_power": %.4f, "wallet_address": "%s", "balance": %.4f}' \
 "$moniker" \
 "$identity" \
 "$website" \
@@ -163,6 +172,7 @@ main() {
 "$node_version" \
 "$latest_block_height" \
 "$catching_up" \
+"$validator_moniker" \
 "$validator_address" \
 "$explorer_url" \
 "$validator_pub_key" \
@@ -191,6 +201,7 @@ main() {
 			printf_n "$t_sy3"
 		fi
 		
+		printf_n "$t_vm" "$validator_moniker"
 		printf_n "$t_va" "$validator_address"
 		printf_n "$t_eu" "$explorer_url"
 		printf_n "$t_pk" "$validator_pub_key"	
